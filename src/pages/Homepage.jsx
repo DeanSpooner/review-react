4a8b7178738ce2ed300b8643b826de449263dc84
Dean Spooner <dean.spooner@deptagency.com>
2022-04-21 11:45:54 +0100

    Configured for GraphQL.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import useFetch from '../hooks/useFetch';
 import { Link } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
+
+const REVIEWS = gql`
+  query GetReviews {
+    reviews {
+      data {
+        id
+        attributes {
+          title,
+          rating,
+          body
+        }
+      }
+    }
+  }
+`
 
 const Homepage = () => {
-  const { loading, error, data } = useFetch(
-    `http://localhost:1337/api/reviews`
-  );
+
+  const { loading, error, data } = useQuery(REVIEWS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
-  return <div>{data.data.map(review => (
+  return <div>{data.reviews.data.map(review => (
     <div key={review.id} className="review-card">
       <div className="rating">{review.attributes.rating}</div>
       <h2>{review.attributes.title}</h2>
